9e2ddf645b829d51786b2ecd2f473e6603f87abe
anahitahassan <anahitarhassan@gmail.com>
2020-08-23 13:10:29 -0700

    changed a bunch of stuff

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
+var slideIndex = 0;
+showSlides();
+//add the global timer variable
+var slides,dots,timer;
 
-var i=1;
-function changeImage(){
-	if(i==1){
-		document.getElementById("sm").src="https://uploads-ssl.webflow.com/5d4868dce260e32178361040/5d81014571af816587868d3d_FTC.png";
-		i=2;
-	}
-	else if(i==2){
-		document.getElementById("sm").src="https://longislandweekly.com/wp-content/uploads/2019/02/FTC-championship-photo.jpg";
-		i=3;
-	}
-	else if(i==3){
-		document.getElementById("sm").src="https://i0.wp.com/njstempathways.org/wp-content/uploads/2020/01/DSCF5553.jpg?fit=4608%2C2592&ssl=1";
-		i=4;
-	}
-	else if(i==4){
-		document.getElementById("sm").src="https://s.hdnux.com/photos/01/10/06/22/18900106/3/920x920.jpg";
-		i=5;
-	}
-	else if(i==5){
-		document.getElementById("sm").src="https://www.qualcomm.com/sites/ember/files/styles/optimize/public/blog/managed-images/the-kit-rover-image-4_sized.jpg?itok=UFieP3pV";
-		i=1;
-	}
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    //put the timeout in the timer variable
+    timer = setTimeout(showSlides, 2000); // Change image every 8 seconds
 }
-function startTime(){
-	var timer = setInterval("changeImage()",10000);
+
+function currentSlide(index) {
+    //clear/stop the timer
+    clearTimeout(timer);
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    //set the slideIndex with the index of the function
+    slideIndex = index;
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index-1].style.display = "block";  
+    dots[index-1].className += " active";
+    //create a new timer
+    timer = setTimeout(showSlides, 4000);
 }
+
+
